24d67cc5bb6b4b3054e1ef9b983e4132e4457324
Nicolashe <nicolashe@thinkgeo.com>
2016-01-20 13:35:30 +0800

    add code to support ng-cloak.

--- ngCloakDemo/cloakDemo.js
+++ ngCloakDemo/cloakDemo.js
@@ -5,12 +5,12 @@
 (function () {
 
     angular.module('bk.demo', []);
+    angular.module('bk.demo').controller('demoController', demoController);
 
-    angular.module('bk.demo').controller('demoController', demoController)
-
-    demoController.$inject = ['$scope']
-
-    function demoController($scope) {
-        $scope.demoTitle = 'wtf..................wtf..........................wtf........................';
+    demoController.$inject = ['$scope', '$timeout'];
+    function demoController($scope, $timeout) {
+        $timeout(function () {
+            $scope.demoTime = Date.now();
+        }, 1000)
     }
 })()
