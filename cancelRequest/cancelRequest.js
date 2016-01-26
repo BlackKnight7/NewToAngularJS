9797775c6cf3e2df9d39c4489f9efb6a03f04cd7
Nicolashe <nicolashe@thinkgeo.com>
2016-01-26 15:30:40 +0800

    Create cancel request demo.

--- cancelRequest/cancelRequest.js
+++ cancelRequest/cancelRequest.js
@@ -1,3 +1,27 @@
 /**
  * Created by tgdev on 26/01/16.
  */
+
+
+(function () {
+
+    angular.module('bk.demo', []);
+    angular.module('bk.demo').controller('demoController', demoController);
+
+    demoController.$inject = ['$scope', '$timeout', 'DataFactory'];
+    function demoController($scope, $timeout, DataFactory) {
+        var getDataRequest;
+
+        $scope.cancel = function () {
+            getDataRequest.abort();
+        }
+
+        $scope.send = function () {
+            getDataRequest = DataFactory.getData();
+            getDataRequest.then(function (result) {
+                // do something with result.
+                console.log(result);
+            })
+        }
+    }
+})()
--- cancelRequest/dataFactory.js
+++ cancelRequest/dataFactory.js
@@ -1,3 +1,47 @@
 /**
  * Created by tgdev on 26/01/16.
  */
+
+(function () {
+
+    angular.module('bk.demo').factory('DataFactory', function ($http, $q) {
+        function getData(id) {
+            var deferredAbort = $q.defer();
+
+            var request = $http({
+                method: 'get',
+                url: 'http://localhost:3000/api/v1/healthcheck',
+                timeout: deferredAbort.promise
+            });
+
+            var promise = request.then(
+                function (response) {
+                    return ( response.data );
+                },
+                function () {
+                    return ( $q.reject('Something went wrong') );
+                }
+            );
+
+            promise.abort = function () {
+                deferredAbort.resolve();
+            };
+
+            promise.finally(
+                function () {
+                    promise.abort = angular.noop;
+                    deferredAbort = request = promise = null;
+                }
+            );
+
+            return ( promise );
+        }
+
+        return ({
+            getData: getData
+        });
+
+    });
+})();
+
+
